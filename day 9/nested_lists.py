f25350c583f017a1b3414fb8c91a80fbef7a7f18
sam-mugo <keithgety@gmail.com>
2021-10-12 19:56:30 +0300

    comments for easier readability

--- day 9/nested_lists.py
+++ day 9/nested_lists.py
@@ -6,15 +6,21 @@ if __name__ == '__main__':
         
         student_grades.append([name, score])
         
-        
+    #new list containing the scores only
     scores = [el[1] for el in student_grades]
+    #remove duplicate scores
     score_set = set(scores)
+    #convert to a list of unique scores
     unique_scores = list(score_set)
 
+    #find minimum score
     lowest = min(unique_scores)
+    #remove min score to get min-score
     unique_scores.remove(lowest)
     
+    #sort alphabetically
     student_grades.sort()
+    
     for i in student_grades:
         lowest2 = min(unique_scores)
         if i[1] == lowest2:
