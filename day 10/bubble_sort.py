6b7158add5f9c7474afc6c0e0d36ddfc4eef7d03
sam-mugo <keithgety@gmail.com>
2021-10-15 23:06:44 +0300

    comments for readability

--- day 10/bubble_sort.py
+++ day 10/bubble_sort.py
@@ -1,4 +1,5 @@
-def bubble_sort(arr):
+def bubble_sort(arr):    
+    #len(arr) -1 the last item cant compare to any other so its the max
     indexing = len(arr) -1
     sorted = False
     
@@ -7,6 +8,7 @@ def bubble_sort(arr):
         for i in range(0, indexing):
             if arr[i] > arr[i + 1]:
                 sorted = False
+                #swap elements
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
     return arr
             
